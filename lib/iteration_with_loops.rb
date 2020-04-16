61c750500d76f4593710bee1694160041f45cc9b
David Chung <dbchung3@yahoo.com>
2020-04-16 01:22:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     min_string = ""
     while element_index < src[row_index].count do 
-      if src[row_index][element_index].count < min_string.length 
+      if src[row_index][element_index].length < min_string.length 
         min_string = src[row_index][element_index]
       end 
       element_index +=1 
